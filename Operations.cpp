442057afdd277a29de1ea826e1b8919ef02974da
JeremyR <kidnip@knight.ucf.edu>
2021-11-28 03:47:25 -0500

    second commit

--- Operations.cpp
+++ Operations.cpp
@@ -1,7 +1,26 @@
 #include "std_lib_facilities.h"
 
 int main(){
+    string operation;
+    double input1, input2, result = 0;
+    
+    cout << "Enter an operation and two values (separated by spaces):\n";
+    cin >> operation >> input1 >> input2;
 
+    if(operation == "+" || operation == "plus"){
+        result = input1 + input2;
+    }
+    else if(operation == "-" || operation == "minus"){
+        result = input1 - input2;
+    }
+    else if(operation == "*" || operation == "mul"){
+        result = input1 * input2;
+    }
+    else if(operation == "/" || operation == "div"){
+        result = input1 / input2;
+    }
+
+    cout << "The result is " << result;
 
     return 0;
 }
